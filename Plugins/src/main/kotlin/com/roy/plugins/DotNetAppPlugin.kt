29be8de460de57d596a9a95c29d73c29ef4420f5
Roy Dammarell <roy.dammarell@faithlife.com>
2021-10-20 08:13:10 -0700

    Add app plugin

---
+++ Plugins/src/main/kotlin/com/roy/plugins/DotNetAppPlugin.kt
@@ -0,0 +1,11 @@
+package com.roy.plugins
+
+import com.android.build.gradle.AppExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DotNetAppPlugin : Plugin<Project> {
+  override fun apply(target: Project) {
+    target.extensions.findByType(AppExtension::class.java)!!.aaptOptions.noCompress("dll", "dll.config", "pdb", "mdb", "mj", "jm", "environment")
+  }
+}
